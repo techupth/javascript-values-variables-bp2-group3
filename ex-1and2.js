fe96a1ca158780bfcd9ca38652ebdfb43f72fa90
planohave <planohavev2@gmail.com>
2023-12-02 02:46:43 +0700

    feat:homework

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,20 @@
 //Exercise #1: Variable Creation
 //Start coding here
+let Name = "Karter";
+let Surname = "Anderson";
+let Address = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai";
+const Resterant Name = "Chef's Table by Chef James";
+const total Bill = 15000;
+const total Open Resterant = 1500;
+
 
 //Exercise #2: Variable Reassignment
 //Start coding here
+Address = "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641”
+console.log (Address); //"(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641”
+total Bill = 5000;
+console.log (total Bill); //2500
+total Open Resterant =2500;
+console.log (Open Resterant); //2500
+
+
--- ex-3.js
+++ ex-3.js
@@ -1,6 +1,7 @@
 // Exercise #3: Celsius to Fahrenheit
 let celsius = 30;
-
 // Start coding here;
+celsius x 1.8+ 32; //86
+let fahrenheit; 86;
+console.log (fahrenheit); //86
 
-let fahrenheit;
